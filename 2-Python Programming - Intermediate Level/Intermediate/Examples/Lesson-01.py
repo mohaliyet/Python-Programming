e1aa438299a89f309332e5131861840b3907a1d5
Mohammed Aliy <mohaliyet@gmail.com>
2024-10-14 06:56:36 +0300

    lesson 1 example updated

--- 2-Python Programming - Intermediate Level/Intermediate/Examples/Lesson-01.py
+++ 2-Python Programming - Intermediate Level/Intermediate/Examples/Lesson-01.py
@@ -1,18 +1,22 @@
+## Example #1
 # matrix = [[1, 2, 3], 
 #           [4, 5, 6], 
 #           [7, 8, 9]]
 
 # print(matrix[0][2])
 
+## Example #2
 # sqr = []
 
 # for i in range(11):
 #     sqr.append(i**2)
-
-# sqr = [i**2 for i in range(11)]
-
 # print(sqr)
 
+## Example #3
+# sqr = [i**2 for i in range(11)]
+# print(sqr)
+
+## Example #4
 # nested_list = [[1, 2, 3],
 #                [4, 5, 6], 
 #                [7, 8, 9]]
@@ -23,6 +27,7 @@
 #                   for i in row] 
 # print(flattened_list) 
 
+# Example #5
 even_sqr = [i**2 
             for i in range(11) 
             if i % 2 == 0]
